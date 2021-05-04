ef7d1f170ad69efe63ecca7942be1a554e36e13b
Carlos <darksunreaper@gmail.com>
2021-05-04 19:18:48 +0200

    checkboxes exe 1 hecho

--- CFGS Desarrollo de Aplicaciones Web/M03_Programación/UF5/Pt_Swing/src/Checkboxes.java
+++ CFGS Desarrollo de Aplicaciones Web/M03_Programación/UF5/Pt_Swing/src/Checkboxes.java
@@ -5,14 +5,16 @@ public class Checkboxes extends JPanel{
     JCheckBox[] checkBoxes = new JCheckBox[4];
 
     public Checkboxes() {
-        iniciarComponentes();
-        setLayout(new GridLayout(4, 1));
+        iniciarComponentes(); // Iniciamos las checkboxes
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); // Hacemos que el layout sea un BoxLayout de arriba a abajo
+        this.setBackground(Color.WHITE); // Cambiamos el fondo de el JPanel a blanco
         for (int i = 0; i < checkBoxes.length; i++){
-            add(checkBoxes[i]);
+            checkBoxes[i].setBackground(Color.WHITE); // Cambiamos el fondo de cada boton a blanco
+            add(checkBoxes[i]); // Generamos todas las checboxes
         }
     }
 
-    public void iniciarComponentes(){
+    public void iniciarComponentes(){ // Nos genera 4 checkboxes con 4 etiquetas diferentes
         checkBoxes[0] = new JCheckBox("Color");
         checkBoxes[1] = new JCheckBox("Negreta");
         checkBoxes[2] = new JCheckBox("Inclinada");
--- CFGS Desarrollo de Aplicaciones Web/M03_Programación/UF5/Pt_Swing/src/VentanaCheckboxes.java
+++ CFGS Desarrollo de Aplicaciones Web/M03_Programación/UF5/Pt_Swing/src/VentanaCheckboxes.java
@@ -6,63 +6,81 @@ import java.awt.event.ItemListener;
 public class VentanaCheckboxes extends JFrame {
     private Checkboxes checkboxes;
     private JLabel texto;
-    public VentanaCheckboxes(){
-        checkboxes = new Checkboxes();
-        texto = new JLabel("Jugem amb checkBoxes....", SwingConstants.CENTER);
-        texto.setFont(new Font("Arial", Font.PLAIN, 16));
-        texto.setOpaque(true);
-        texto.setBackground(Color.WHITE);
-        setTitle("Checkboxes");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    private int size = 16, estilo = Font.PLAIN;
 
-        setSize(540, 140);
+    public VentanaCheckboxes(){
+        checkboxes = new Checkboxes(); // Iniciamos las checkboxes en la ventana
+
+        texto = new JLabel("Jugem amb checkBoxes....", SwingConstants.CENTER); // Generamos el JLabel para que aparezca el texto
+        texto.setFont(new Font("Arial", estilo, size)); // Le ponemos la fuente que queremos al texto
+        texto.setOpaque(true); // Aplicamos que se pueda cambiar el color al JLabel
+        texto.setBackground(Color.WHITE); // Cambiamos el color del JLabel a blanco
+
+        setTitle("Checkboxes"); // Cambiamos el titulo de la aplicacion
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // Hacemos que cuando se cierre la ventana de la aplicacion, esta tambien se acabe
+        setSize(540, 140); // Ponemos el tamaño de la ventana
+        // Generamos el toolkit para poner un icono y colocar la pantalla al medio para cuando inicie
         Toolkit pantalla = Toolkit.getDefaultToolkit();
         setIconImage(pantalla.getImage("src/checkbox_verde.png"));
         setLocation(pantalla.getScreenSize().width / 2 - getWidth() / 2, pantalla.getScreenSize().height / 2 - getHeight() / 2);
+
+        // Añadimos los metodos de cada checkbox
         checkboxes.checkBoxes[0].addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == 1) {
+                if (e.getStateChange() == 1) { // Si se enciende cambiara el background de la aplicacion a rosa
                     texto.setBackground(Color.PINK);
-                } else {
+                    checkboxes.setBackground(Color.PINK);
+                    for (JCheckBox checkbox:checkboxes.checkBoxes) {
+                        checkbox.setBackground(Color.PINK);
+                    }
+                } else { // Cuando se apaga vuelve el background a blanco
                     texto.setBackground(Color.WHITE);
+                    checkboxes.setBackground(Color.WHITE);
+                    for (JCheckBox checkbox:checkboxes.checkBoxes) {
+                        checkbox.setBackground(Color.WHITE);
+                    }
                 }
             }
         });
         checkboxes.checkBoxes[1].addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == 1) {
-                    texto.setFont(new Font("Arial", Font.BOLD, 16));
-                } else {
-                    texto.setFont(new Font("Arial", Font.PLAIN, 16));
+                if (e.getStateChange() == 1) { // Si se activa la letra del JPanel se pone en negrita
+                    estilo += Font.BOLD;
+                } else { // Al desactivarla se quita la negrita
+                    estilo -= Font.BOLD;
                 }
+                texto.setFont(new Font("Arial", estilo, size));
             }
         });
         checkboxes.checkBoxes[2].addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == 1) {
-                    texto.setFont(new Font("Arial", Font.ITALIC, 16));
-                } else {
-                    texto.setFont(new Font("Arial", Font.PLAIN, 16));
+                if (e.getStateChange() == 1) { // Si se activa la letra del JPanel se pone en cursiva
+                    estilo += Font.ITALIC;
+                } else { // Al desactivarla se desactiva la cursiva
+                    estilo -= Font.ITALIC;
                 }
+                texto.setFont(new Font("Arial", estilo, size));
             }
         });
         checkboxes.checkBoxes[3].addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == 1) {
-                    texto.setFont(new Font("Arial", Font.PLAIN, 32));
-                } else {
-                    texto.setFont(new Font("Arial", Font.PLAIN, 16));
+                if (e.getStateChange() == 1) { // Al activarlo hace que la fuente de letra sea mayor
+                    size = 32;
+                } else { // Al desactivarla la fuente vuelve a la mida normal
+                    size = 16;
                 }
+                texto.setFont(new Font("Arial", estilo, size));
             }
         });
 
+        // Ponemos los objetos en nuestra ventana y la habilitamos
         add(checkboxes, BorderLayout.EAST);
-
         add(texto, BorderLayout.CENTER);
+
         setResizable(false);
         setVisible(true);
     }
